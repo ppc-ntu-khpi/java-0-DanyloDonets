7e0ad2074f8466d565601d859878ee4e31aa68b7
Danylo Donets <EndoBroYT@gmail.com>
2022-01-26 21:26:44 +0200

    Update Shirt.java

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,16 +1,30 @@
 public class Shirt {
-  public int shirtID = 5; // стандартне значення номера моделі сорочки
-  public String description = "-description required-"; // стандартний опис сорочки
+  public int shirtID = 582; // стандартне значення номера моделі сорочки
+  public String description = "Cool oversize shirt for real gansta"; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-  public char colorCode = 'B';
-  public double price = 150.0; // стандартна вартість сорочки
-  public int quantityInStock = 20; // стандартна кількість на складі
+  public char colorCode = 'R';
+  public double price = 25.99; // стандартна вартість сорочки
+  public int quantityInStock = 25; // стандартна кількість на складі
   
+
   // цей метод просто виводить всю інформацію про сорочку на екран
   public void displayShirtInformation() {
     System.out.println("Shirt ID: " + shirtID);
     System.out.println("Shirt description:" + description);
-    System.out.println("Color Code: " + colorCode);
+     switch (colorCode) {
+            case 'R':
+                 System.out.println("Color Code: Red");
+                  break;
+            case 'B':
+                System.out.println("Color Code: Blue");
+                break;
+            case 'G':
+                System.out.println("Color Code: Green");
+                break;
+            default:
+                System.out.println("Color Code: Undefined");
+                break;
+        };
     System.out.println("Shirt price: $" + price);
     System.out.println("Quantity in stock: " + quantityInStock);
   } // кінець методу displayShirtInformation
